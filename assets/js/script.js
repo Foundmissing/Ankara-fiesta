95c6dec4f1dd28aa245443bf9dc08ad0bc450292
Found Missing <foundmissing010@gmail.com>
2023-08-03 14:32:34 +0100

    ankara-fiesta

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElemArr = [overlay, navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * add active class on header when scrolled 200px from top
+ */
+
+// const header = document.querySelector("[data-header]");
+
+// window.addEventListener("scroll", function () {
+//   window.scrollY >= 200 ? header.classList.add("active")
+//     : header.classList.remove("active");
+// })
+
+
+
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
+
+
+
+
+/**
+ * scroll reveal effect
+ */
+
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 2) {
+      sections[i].classList.add("active");
+    }
+  }
+}
+
+scrollReveal();
+
+addEventOnElem(window, "scroll", scrollReveal);
+
+
+const sectio = document.querySelectorAll("[data-sectio]");
+
+const scrollRevea = function () {
+  for (let i = 0; i < sectio.length; i++) {
+    if (sectio[i].getBoundingClientRect().top < window.innerHeight / 2) {
+      sectio[i].classList.add("active");
+    }
+  }
+}
+
+scrollRevea();
+
+addEventOnElem(window, "scroll", scrollRevea);
